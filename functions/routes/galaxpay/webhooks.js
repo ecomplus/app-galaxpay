3fa417199132c9632493525e7aadc6e76d6470b3
wisley7l <wisley7lagoas@yahoo.com.br>
2022-03-04 10:01:20 -0300

    fix(webhooks): back functions

--- functions/routes/galaxpay/webhooks.js
+++ functions/routes/galaxpay/webhooks.js
@@ -18,7 +18,26 @@ exports.post = ({ appSdk, admin }, req, res) => {
   const collectionTransaction = admin.firestore().collection('transactions')
 
   const addTransactionFireBase = (Transaction) => {
-    
+    console.log('>  Transaction ID ', Transaction.galaxPayId)
+    admin.firestore().collection('transactions').doc(String(Transaction.galaxPayId))
+      .set(Transaction)
+      .catch(console.error)
+  }
+
+  const createDocFireBase = () => {
+    console.log('> Function create')
+    const subscription = collectionSubscription.doc(subscriptionId)
+    subscription.get()
+      .then((documentSnapshot) => {
+        const storeId = documentSnapshot.data().store_id
+        if (documentSnapshot.exists && storeId) {
+          const transaction = galaxpayHook.Transaction
+          res.status(200).send(`SUCCESS  ${storeId}`)
+          addTransactionFireBase(transaction)
+        } else {
+          res.status(404).send('NOT FOUND Doc Subscription in Firebase')
+        }
+      })
   }
 
   if (galaxpayHook.confirmHash) {
@@ -43,45 +62,9 @@ exports.post = ({ appSdk, admin }, req, res) => {
       })
       .then(data => {
         if (!data) {
-          console.log('> Function create')
-          const subscription = collectionSubscription.doc(subscriptionId)
-          subscription.get()
-            .then((documentSnapshot) => {
-              const storeId = documentSnapshot.data().store_id
-              if (documentSnapshot.exists && storeId) {
-                const transaction = galaxpayHook.Transaction
-                return transaction
-              } else {
-                return false
-              }
-            })
-            .then(data => {
-              if (!data) {
-                console.log('>  Transaction ID ', data.galaxPayId)
-                admin.firestore().collection('transactions').doc(String(data.galaxPayId))
-                  .set(data)
-                  .then((data) => {
-                    console.log('> ok ', data)
-                    return data
-                  })
-                  .then((data) => {
-                    if (!data) {
-                      res.status(200).send('SUCCESS')
-                    } else {
-                      res.status(404).send('NOT FOUND!')
-                    }
-                  })
-                  .catch((err) => {
-                    console.error()
-                    res.status(404).send(`NOT FOUND!  ${err}`)
-                  })
-              }
-            })
-            .catch((err) => {
-              console.error()
-              res.status(404).send(`NOT FOUND!  ${err}`)
-            })
+          createDocFireBase()
         }
       })
+      .catch(console.error)
   }
 }
