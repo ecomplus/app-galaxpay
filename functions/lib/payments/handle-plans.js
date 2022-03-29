c929657b9094b0539181f0618dec1f90d9192317
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-03-29 17:56:39 -0300

    refactor(handle plans): verifing replace lable plan

--- functions/lib/payments/handle-plans.js
+++ functions/lib/payments/handle-plans.js
@@ -29,7 +29,14 @@ const handlePlanTransction = (label, appData) => {
     }
     return plan
   } else if (appData.plans) {
-    return appData.plans.find((plan) => (plan.label ? plan.label : 'Plano' + ' ' + plan.periodicity) === label)
+    console.log('> label ' + label)
+    for (let i = 0; i < appData.plans.length; i++) {
+      const planName = appData.plan[i].label ? appData.plan[i].label : 'Plano'
+      console.log(planName + ' ' + appData.plan[i].periodicity)
+      if (planName + ' ' + appData.plan[i].periodicity === label.trim()) {
+        return appData.plan[i]
+      }
+    }
   }
 }
 
