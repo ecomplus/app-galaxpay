ef1c74100cc886738357340c65785126c690db07
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-04-07 17:07:44 -0300

    refactor(Authorization) back authorization

--- functions/lib/galaxpay/create-access.js
+++ functions/lib/galaxpay/create-access.js
@@ -1,39 +1,17 @@
 const createAxios = require('./create-axios')
 const auth = require('./create-authorization')
-let { ID_GALAXPAY_PARTNER, HASH_GALAXPAY_PARTNER } = process.env
+const { ID_GALAXPAY_PARTNER, HASH_GALAXPAY_PARTNER } = process.env
 
 module.exports = function (galaxpayId, galaxpayHash, isSandbox, storeId, firestoreColl = 'galaxpay_tokens') {
   const self = this
 
   let documentRef
-
   const hashLogin = Buffer.from(`${galaxpayId}:${galaxpayHash}`).toString('base64')
-
-  const firestorePartner = 'galaxpay_partners'
-  const docPartner = require('firebase-admin').firestore().doc(`${firestorePartner}/ecomplus`)
-  let hashPartner
 
   if (firestoreColl) {
     documentRef = require('firebase-admin')
       .firestore()
       .doc(`${firestoreColl}/${storeId}-${hashLogin}`)
-  }
-
-  if (ID_GALAXPAY_PARTNER && HASH_GALAXPAY_PARTNER) {
-    hashPartner = Buffer.from(`${ID_GALAXPAY_PARTNER}:${HASH_GALAXPAY_PARTNER}`).toString('base64')
-  } else {
-    if (docPartner) {
-      docPartner.get()
-        .then((documentSnapshot) => {
-          const idGalaxpayPartner = documentSnapshot.data().galaxpayId
-          const hashGalaxpayPartner = documentSnapshot.data().galaxpayHash
-          if (documentSnapshot.exists && idGalaxpayPartner && hashGalaxpayPartner) {
-            ID_GALAXPAY_PARTNER = process.env.ID_GALAXPAY_PARTNER = idGalaxpayPartner
-            HASH_GALAXPAY_PARTNER = process.env.HASH_GALAXPAY_PARTNER = hashGalaxpayPartner
-            hashPartner = Buffer.from(`${idGalaxpayPartner}:${hashGalaxpayPartner}`).toString('base64')
-          }
-        })
-    }
   }
 
   this.preparing = new Promise((resolve, reject) => {
@@ -44,9 +22,10 @@ module.exports = function (galaxpayId, galaxpayHash, isSandbox, storeId, firesto
     }
 
     const handleAuth = (isSandbox) => {
-      console.log('> Galaxpay Auth02 >', hashPartner)
-      auth(hashLogin, isSandbox, hashPartner)
+      console.log('> Galaxpay Auth02')
+      auth(hashLogin, isSandbox)
         .then((accessToken) => {
+          console.log(`> Galaxy token: ${hashLogin}`)
           authenticate(accessToken, isSandbox)
           if (documentRef) {
             documentRef.set({ accessToken }).catch(console.error)
--- functions/lib/galaxpay/create-authorization.js
+++ functions/lib/galaxpay/create-authorization.js
@@ -1,24 +1,15 @@
-module.exports = (hashLogin, isSandbox, hashPartner) => new Promise((resolve, reject) => {
+module.exports = (hashLogin, isSandbox) => new Promise((resolve, reject) => {
   // https://docs.galaxpay.com.br/autenticacao
   // https://docs.galaxpay.com.br/auth/token
   let accessToken
   const axios = require('./create-axios')(accessToken, isSandbox)
   const request = isRetry => {
-    const headers = { Authorization: `Basic ${hashLogin}` }
-
-    console.log('> hash Partner: ', hashPartner, ' isSandBox ', isSandbox)
-    if (hashPartner && hashPartner !== '' && !isSandbox) {
-      headers.AuthorizationPartner = hashPartner
-    }
-
-    console.log('> header ', { headers })
     axios.post('/token', {
       grant_type: 'authorization_code',
       scope: 'customers.read customers.write plans.read plans.write transactions.read transactions.write webhooks.write cards.read cards.write card-brands.read subscriptions.read subscriptions.write charges.read charges.write boletos.read'
-    }, { headers })
+    }, { headers: { Authorization: `Basic ${hashLogin}` } })
       .then(({ data }) => resolve(data.access_token))
       .catch(err => {
-        console.log('> Err ', err)
         if (!isRetry && err.response && err.response.status >= 429) {
           setTimeout(() => request(true), 7000)
         }
