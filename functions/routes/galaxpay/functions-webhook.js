b7e83c89516bdacc237af8775261ce6f132f7961
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-04-12 16:42:05 -0300

    refactor(Webhook) edit file function

--- functions/routes/galaxpay/functions-webhook.js
+++ functions/routes/galaxpay/functions-webhook.js
@@ -1,5 +1,29 @@
 const { parseId, parseStatus, parsePeriodicity } = require('../../lib/galaxpay/parse-to-ecom')
 
+const findOrderByTransactionId = (appSdk, storeId, auth, transactionId) => {
+  return new Promise((resolve, reject) => {
+    appSdk.apiRequest(storeId, `/orders.json?transactions._id=${transactionId}`, 'GET', null, auth)
+      .then(({ response }) => {
+        resolve({ response })
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+const findOrderById = (appSdk, storeId, auth, orderId) => {
+  return new Promise((resolve, reject) => {
+    appSdk.apiRequest(storeId, `/orders/${orderId}.json?fields=transactions,financial_status`, 'GET', null, auth)
+      .then(({ response }) => {
+        resolve({ response })
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
 const createTransaction = (appSdk, res, subscription, GalaxPayTransaction, GalaxPaySubscription, subscriptionId) => {
   subscription.get()
     .then((documentSnapshot) => {
