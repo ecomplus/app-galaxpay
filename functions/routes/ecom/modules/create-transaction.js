a52fc2c8d2f7b2d7d33b550ca676b2440fcfc867
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-05-04 12:20:34 -0300

    chore(create-transaction): update log

--- functions/routes/ecom/modules/create-transaction.js
+++ functions/routes/ecom/modules/create-transaction.js
@@ -167,7 +167,7 @@ exports.post = ({ appSdk, admin }, req, res) => {
             return data.data.Subscription
           })
           .then((data) => {
-            console.log(`> New Subscription ${data} <`)
+            console.log(`> New Subscription ${JSON.stringify(data)} <`)
 
             transaction.payment_link = data.paymentLink
 
@@ -184,7 +184,7 @@ exports.post = ({ appSdk, admin }, req, res) => {
               transaction_id: transactionGalaxPay.tid,
               transaction_code: transactionGalaxPay.authorizationCode
             }
-            console.log(`>> Create Transaction:  ${transaction} <<`)
+            console.log(`>> Create Transaction:  ${JSON.stringify(transaction)} <<`)
 
             res.send({
               redirect_to_payment: redirectToPayment,
